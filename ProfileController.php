b986ebd499004e62a350536d7bf62c23cc4372cd
kanai <k.y.h.k08241212@gmail.com>
2022-05-06 01:11:33 +0900

    Laravel_09課題提出

---
+++ ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    //【応用】 Admin/ProfileControllerに、以下のadd, create, edit, update それぞれのActionを追加してみましょう
+    public function add()
+ {
+ return view('admin.profile.create');
+ }
+ public function create()
+ {
+ return redirect('admin/profile/create');
+ }
+
+  public function edit()
+  {
+      return view('admin.profile.edit');
+  }
+
+  public function update()
+  {
+      return redirect('admin/profile/edit');
+  }
+}
